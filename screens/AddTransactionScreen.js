88bee0f4e797cfdd9463aff69c339dc0b37b3236
Fariz <nizaralfariziakbar10@gmail.com>
2018-11-07 20:06:15 +0700

    add feature create transaction

--- screens/AddTransactionScreen.js
+++ screens/AddTransactionScreen.js
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   Keyboard,
   TouchableWithoutFeedback,
+  AsyncStorage,
+  ToastAndroid,
 } from 'react-native';
 import {
   Button,
@@ -11,9 +13,11 @@ import {
   View as ShoutemView,
   getTheme,
   TextInput,
+  DropDownMenu,
 } from '@shoutem/ui';
 import { StyleProvider } from '@shoutem/theme';
 import _ from 'lodash';
+import axios from 'axios';
 
 let theme = _.merge(getTheme(), {
   'shoutem.ui.View': {
@@ -45,35 +49,128 @@ var DismissKeyboard = require('dismissKeyboard');
 
 export default class AddTransactionScreen extends Component {
   static navigationOptions = {
-    title: 'Tambah Transaksi',
+    title: 'Add Transaction',
+  }
+
+  constructor(props){
+  super(props);
+  this.loadApp()
+  this.state = {
+    status: [
+        {
+          name: "Select",
+          value: ""
+        },
+        {
+          name: "Plus",
+          value: "plus"
+        },
+        {
+          name: "Minus",
+          value: "minus"
+        },
+      ],
+    }
+  }
+
+  state = {amount: '', description: '', token: '', success: '', error: []};
+
+  async loadApp() {
+    const apiToken = await AsyncStorage.getItem('apiToken')
+
+    this.setState({token: apiToken})
+  }
+
+  async onButtonPress() {
+    const { selectedStatus, amount, description, token } = this.state;
+
+    var config = {
+        headers: {
+          'Accept': "application/json",
+          'Content-Type' : "application/json",
+          'Authorization' : "Bearer " + this.state.token,
+        }
+    };
+
+    if (this.checkInput(selectedStatus, amount)) {
+
+      await axios.post('http://wangku.herokuapp.com/api/transaction/user', {
+        status: selectedStatus.value,
+        amount: amount,
+        description: description
+      }, config)
+        .then(response => this.setState({
+          success: response.data.data.created
+        }))
+        .catch(error => this.setState({
+          error: error.response.data.errors
+        }));
+
+      if (this.state.error !== undefined) {
+        (this.state.error['status'] != null) ? ToastAndroid.show(this.state.error['status'][0], ToastAndroid.SHORT) : '';
+        (this.state.error['amount'] != null) ? ToastAndroid.show(this.state.error['amount'][0], ToastAndroid.SHORT) : '';
+      } else if (this.state.success != null || this.state.success != '' || this.state.success != undefined) {
+        ToastAndroid.show('Successfully Add Transaction', ToastAndroid.SHORT);
+        this.props.navigation.goBack();
+      }
+
+      this.setState({
+        success: '',
+        error: []
+      });
+
+    } else {
+      ToastAndroid.show('Status & Amount Cannot Empty.', ToastAndroid.SHORT);
+    }
+  }
+
+  checkInput(selectedStatus, amount) {
+    let successInput = false;
+
+    if (selectedStatus !== undefined && amount !== undefined) {
+      if (selectedStatus.value !== '') {
+        this.setState({ zstatus: selectedStatus.value })
+        successInput = true;
+      }
+    }
+
+    return successInput;
   }
 
   render() {
+    const selectedStatus = this.state.selectedStatus || this.state.status[0];
     return (
       <StyleProvider style={theme}>
           <TouchableWithoutFeedback onPress={ () => { DismissKeyboard() } }>
             <ShoutemView styleName="vertical h-center content" >
-              <TextInput
-                placeholder={'Username'}
-                styleName="textInput"
-                autoFocus={true}
+              <DropDownMenu
+                styleName="horizontal"
+                options={this.state.status}
+                selectedOption={selectedStatus ? selectedStatus : this.state.status[0]}
+                onOptionSelected={(status) => this.setState({ selectedStatus: status })}
+                titleProperty="name"
+                valueProperty="status.value"
               />
               <TextInput
-                placeholder={'Email'}
+                placeholder={'Amount'}
                 styleName="textInput"
-                keyboardType="email-address"
+                keyboardType="numeric"
+                value={this.state.amount}
+                onChangeText={ amount => this.setState({ amount }) }
               />
               <TextInput
-                placeholder={'Password'}
-                secureTextEntry
+                placeholder={'Description'}
                 styleName="textInput"
+                value={this.state.description}
+                onChangeText={ description => this.setState({ description }) }
               />
-              <Button styleName="secondary register">
-                <Text>Tambah</Text>
+              <Button styleName="secondary register" onPress={this.onButtonPress.bind(this)}>
+                <Text>Create</Text>
               </Button>
             </ShoutemView>
           </TouchableWithoutFeedback>
       </StyleProvider>
     );
   }
+
 }
--- screens/TransactionsScreen.js
+++ screens/TransactionsScreen.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View as ViewReact,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 import {
   Button,
@@ -19,6 +20,7 @@ import {
 } from '@shoutem/ui';
 import { StyleProvider } from '@shoutem/theme';
 import _ from 'lodash';
+import ActionButton from 'react-native-action-button';
 
 let theme = _.merge(getTheme(), {
   'shoutem.ui.Row': {
@@ -49,22 +51,59 @@ let theme = _.merge(getTheme(), {
 
 
 export default class TransactionsScreen extends Component {
+  state = { email: '', password: '', name: '', usermail: '', meta: '', error: '' };
+
+  async onButtonPress() {
+    const { email, password } = this.state;
+
+    const data = {
+      email,
+      password
+    };
+
+    if (this.checkInput(email, password)) {
+      await axios.post('http://wangku.herokuapp.com/api/login', data)
+        .then(response => this.setState({
+          name: response.data.data.name,
+          usermail: response.data.data.email,
+          meta: response.data.meta.token,
+          error: response.data.error
+        }))
+        .catch(error => console.warn(error));
+      if (this.state.error != null) {
+        ToastAndroid.show(this.state.error, ToastAndroid.SHORT);
+      } else {
+        ToastAndroid.show('Welcome ' + this.state.name, ToastAndroid.SHORT);
+        this.login();
+      }
+    } else {
+      ToastAndroid.show('Email & Password Cannot Empty.', ToastAndroid.SHORT);
+    }
+  }
+
   render() {
     return (
       <StyleProvider style={theme}>
         <ViewReact style={{ flex: 1, backgroundColor: 'white' }}>
-        <Heading styleName="header">Wangku</Heading>
-          <View styleName="vertical">
-            <TouchableOpacity>
-              <Row styleName="container">
-                <View styleName="vertical space-between content">
-                  <Subtitle>Sangu dari Orang Tua </Subtitle>
-                  <Caption>June 21  ·  04:00</Caption>
-                </View>
-                <Button styleName="right-icon"><Icon name="plus-button" style={{ color: 'green' }} /><Text styleName="plus">10.000</Text></Button>
-              </Row>
-            </TouchableOpacity>
-          </View>
+          <Heading styleName="header">Wangku</Heading>
+          <ScrollView style={{ flex: 1 }}>
+            <View styleName="vertical">
+              <TouchableOpacity>
+                <Row styleName="container">
+                  <View styleName="vertical space-between content">
+                    <Subtitle>Sangu dari Orang Tua </Subtitle>
+                    <Caption>June 21  ·  04:00</Caption>
+                  </View>
+                  <Button styleName="right-icon"><Icon name="plus-button" style={{ color: 'green' }} /><Text styleName="plus">10.000</Text></Button>
+                </Row>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+          <ActionButton
+            buttonColor="rgba(231,76,60,1)"
+            verticalOrientation="up"
+            onPress={() => this.props.navigation.navigate('AddTransaction')}
+          />
         </ViewReact>
       </StyleProvider>
     );
